4d9883c2e81c3e4aed671193bc8a147c15e58a0e
Haukur Már Böðvarsson <haukurmar@gmail.com>
2019-03-23 15:00:48 +0000

    Reference SomeComponent in App

--- packages/app/App.js
+++ packages/app/App.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from 'react'
+import SomeComponent from '@haukurmar/ui'
+
 
 const App = (props) => {
 	return (
 		<Fragment>
 			App
+			<SomeComponent />
 		</Fragment>
 	)
 }
